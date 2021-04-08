dfa27e4936870aeb65b256d10faa9644b8f29d7d
kyeongsu park <35827718+ggorr@users.noreply.github.com>
2021-04-08 14:29:05 +0900

    Create ptbreader.py

---
+++ IDL/Ch04/ptbreader.py
@@ -0,0 +1,32 @@
+import collections
+
+import tensorflow.compat.v1 as tf
+
+
+def readWords(filename: str) -> list:
+	f = tf.io.gfile.GFile(filename, "r")
+	return f.read().replace("\n", "<eos>").split()
+
+
+def buildVocab(filename='ptbdataset/ptb.train.txt') -> dict:
+	data = readWords(filename)
+
+	counter = collections.Counter(data)
+	pairs = sorted(counter.items(), key=lambda x: (-x[1], x[0]))
+
+	words, _ = list(zip(*pairs))
+	wordId = dict(zip(words, range(len(words))))
+	return wordId
+
+
+def fileToWordIds(filename: str, wordId: dict) -> list:
+	data = readWords(filename)
+	return [wordId[word] for word in data if word in wordId]
+
+
+def loadWordIdsFromFiles():
+	wordId = buildVocab()
+	trainData = fileToWordIds('ptbdataset/ptb.train.txt', wordId)
+	validData = fileToWordIds('ptbdataset/ptb.valid.txt', wordId)
+	testData = fileToWordIds('ptbdataset/ptb.test.txt', wordId)
+	return trainData, validData, testData, wordId
